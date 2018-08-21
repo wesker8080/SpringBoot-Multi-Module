ee34a1adcb8d3bbd8b000a9186f69dd3d913d95a
wesker8080 <wesker8080@foxmail.com>
2018-08-21 11:48:16 +0800

    commit

---
+++ et8080-api/src/main/java/com/eliteai/et8080/Api.java
@@ -0,0 +1,8 @@
+package com.eliteai.et8080;
+
+/**
+ * @author MR.ZHANG
+ * @create 2018-08-21 11:45
+ */
+public class Api {
+}
---
+++ et8080-common/src/main/java/com/eliteai/et8080/Utils.java
@@ -0,0 +1,8 @@
+package com.eliteai.et8080;
+
+/**
+ * @author MR.ZHANG
+ * @create 2018-08-21 11:46
+ */
+public class Utils {
+}
